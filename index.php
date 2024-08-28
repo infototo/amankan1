e8552072c812c3ecbbf7c9d86d6372a14d108ddc
infototo <parlinolxtoto@gmail.com>
2024-08-28 10:47:59 +0700

    index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv='refresh' content='0; URL=https://theplumagency.com/'>
+<meta http-equiv='refresh' content='0; URL=https://whitehartwelwyn.co.uk/'>
 </head>
 <body>
 </body>
